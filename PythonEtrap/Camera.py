c8ef6be131e17d5029b7e7f9065d6dc67dd7ba66
Lucas Zunho <lucaszunho17@gmail.com>
2023-03-14 15:20:32 -0300

    Adicionado função de crop & conversão da array de imagem

--- PythonEtrap/Camera.py
+++ PythonEtrap/Camera.py
@@ -27,6 +27,8 @@ class Camera:
         self.picam.configure(self.picam_config)
         
     def startPlateCapture(self, trapNum):
+        self.trapNum = trapNum
+
         # Start picamera
         self.picam.start()
         
@@ -43,25 +45,45 @@ class Camera:
             if key == ord("q"):
                 break
             elif key == ord("p"):
-                # Get image from frame numpy array
-                img_conv = Image.fromarray(img)
-                
-                # Getting user insect name (GUI)
-                bug_name = simpledialog.askstring(
-                    title="Etrap - Loop de captura",
-                    prompt="Qual o nome do inseto? (Acesso rapido: 1=>Afideo; 2=>Parasitoide)")
-                
-                if bug_name == '1':
-                    bug_name = "afideo"
-                elif bug_name == '2':
-                    bug_name = "parasitoide"
-                
-                # Saving image file
-                img_string = "{}_p{}_{}_{}.png".format(
-                    str(date.today()).replace("-", ""), 
-                    trapNum,
-                    bug_name,
-                    self.lastImageId)
-                img_conv.save(img_string)
+                self.imageShot(img)
                 c += 1
         cv2.destroyAllWindows()
+
+    def imageShot(self, img):
+        ''' Take a image shot from opencv camera module (picamera numpy array)
+            @arg img ==> Numpy array
+        '''
+         # Get image from frame numpy array
+        img_conv = Image.fromarray(img)
+        
+        # Getting user insect name (GUI)
+        bug_name = simpledialog.askstring(
+            title="Etrap - Loop de captura",
+            prompt="Qual o nome do inseto? (Acesso rapido: 1=>Afideo; 2=>Parasitoide)")
+        
+        if bug_name == '1':
+            bug_name = "afideo"
+        elif bug_name == '2':
+            bug_name = "parasitoide"
+        
+        # Saving image file (original)
+        img_string = "{}_p{}_{}_{}".format(
+            str(date.today()).replace("-", ""), 
+            self.trapNum,
+            bug_name,
+            self.lastImageId)
+        img_string_png = img_string + ".png"
+        img_conv.save(img_string_png)
+
+        # Image cropping object
+        mid_img = 608 / 2
+        img_crop = cv2.imread(img_string_png)
+        print("[*] Cropping image '" + img_string_png + "'...\n", img_crop.shape)
+
+        # Image dimensions
+        height, width, c = img_crop.shape
+        print("\theight:" + str(height/2) + "\n\twidth:" + str(width/2))
+
+        # Cropping data shape
+        img_cropped = img_crop[(int)(height/2 - mid_img):(int) (height/2 + mid_img), (int)(width/2 - mid_img): (int) (width/2 + 304)]
+        cv2.imwrite(img_string + "_cropped.jpg", img_cropped)
