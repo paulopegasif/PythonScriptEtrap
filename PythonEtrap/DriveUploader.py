afc19877e8c8702f85c68adf1305dea550345865
Lucas Zunho <lucaszunho17@gmail.com>
2023-03-20 16:57:55 -0300

    Regex & tokens de autenticação ajustados

--- PythonEtrap/DriveUploader.py
+++ PythonEtrap/DriveUploader.py
@@ -6,8 +6,8 @@ from pydrive.drive import GoogleDrive # Pydrive library (pip install pydrive)
 
 class DriveUploader:
     folders = {
-        'cropped': '[FOLDER_ID]',
-        'original': '[FOLDER_ID]'
+        'cropped': '1V_2FZw5X9T828xQedEOvb5l9lsr6Zoac',
+        'original': '1Lzn6tLnREkbgqlCT3tHNDqCrPoUyYZqN'
     }
 
     def __init__(self, gauth):
@@ -16,22 +16,19 @@ class DriveUploader:
 
     def startPlateUpload(self, trapNum):
         img_dir = os.path.abspath("./")
-        search_str = "p{}*cropped.jpg$".format(trapNum)
-        search = re.compile(search_str)
 
         c = 0
         for f in os.listdir(img_dir):
             c += 1
             filename = os.path.join(img_dir, f)
 
-            if search.match(filename):  # Cropped images
-                print("Cropped")
+            if re.search("^*p{}*cropped.jpg$".format(trapNum), filename):  # Cropped images
+                print("-> Sending cropped image :: {}".format(filename))
                 gfile = self.drive.CreateFile({'parents' : [{'id' : self.folders['cropped']}], 'title' : f})
                 gfile.SetContentFile(filename)
                 gfile.Upload()
-
-            else:                       # Original images
-                print("Originals")
+            elif re.search("^*p{}*.png$".format(trapNum), filename):
+                print("-> Sending original image :: {}".format(filename))
                 gfile = self.drive.CreateFile({'parents' : [{'id' : self.folders['original']}], 'title' : f})
                 gfile.SetContentFile(filename)
                 gfile.Upload()
