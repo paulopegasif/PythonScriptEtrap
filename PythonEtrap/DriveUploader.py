1dbdd531851f9ec178deeb1fc183e1f68875030f
Lucas Zunho <lucaszunho17@gmail.com>
2023-03-20 17:13:12 -0300

    Formato de upload com regex alterado

--- PythonEtrap/DriveUploader.py
+++ PythonEtrap/DriveUploader.py
@@ -17,18 +17,14 @@ class DriveUploader:
     def startPlateUpload(self, trapNum):
         img_dir = os.path.abspath("./")
 
-        c = 0
-        for f in os.listdir(img_dir):
-            c += 1
-            filename = os.path.join(img_dir, f)
-
-            if re.search("^*p{}*cropped.jpg$".format(trapNum), filename):  # Cropped images
-                print("-> Sending cropped image :: {}".format(filename))
-                gfile = self.drive.CreateFile({'parents' : [{'id' : self.folders['cropped']}], 'title' : f})
-                gfile.SetContentFile(filename)
-                gfile.Upload()
-            elif re.search("^*p{}*.png$".format(trapNum), filename):
-                print("-> Sending original image :: {}".format(filename))
-                gfile = self.drive.CreateFile({'parents' : [{'id' : self.folders['original']}], 'title' : f})
-                gfile.SetContentFile(filename)
-                gfile.Upload()
+        list_dir = os.listdir(img_dir)
+        for f in re.search("^*p{}*cropped.jpg$".format(trapNum), list_dir):
+            print("-> Sending cropped image :: {}".format(f))
+            gfile = self.drive.CreateFile({'parents' : [{'id' : self.folders['cropped']}], 'title' : f})
+            gfile.SetContentFile(f)
+            gfile.Upload()
+        for f in re.search("^*p{}*.png$".format(trapNum), list_dir):
+            print("-> Sending original image :: {}".format(f))
+            gfile = self.drive.CreateFile({'parents' : [{'id' : self.folders['original']}], 'title' : f})
+            gfile.SetContentFile(f)
+            gfile.Upload()
